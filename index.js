230be902a040e643433f8b3ee5515872a05c5934
MiguelValeraDVP <miguel.valera.97@gmail.com>
2023-06-25 16:38:51 -0300

    add headers

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ const app = express();
 require("dotenv").config();
 
 app.use(cors());
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://groovechat.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use("/api/auth/", userRoutes);
 app.use("/api/messages/", messageRoutes);
