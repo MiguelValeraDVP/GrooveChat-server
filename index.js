66223c232c6a5ddd62c86758c526ab821bae26e4
MiguelValeraDVP <miguel.valera.97@gmail.com>
2023-06-25 16:54:25 -0300

    check missing path

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://groovechat.netlify.app"
+    "https://groovechat.netlify.app/socket.io/"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
